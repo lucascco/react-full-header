cde8db871bd65eb8e65eb1081b3604d842e27804
Lucas Carvalho <lucasccorrea@gmail.com>
2018-11-07 12:31:23 -0200

    create stories and the first test to Main

--- stories/Main.js
+++ stories/Main.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import Component from '../src/Main'; // This is our component
+import FullHeader from '../src/Main'; // This is our component
 import { storiesOf } from '@storybook/react';
 
-storiesOf('Component', module)
-    .add('with lyef name', () => (
-        <Component name="lyef" />
+storiesOf('FullHeader', module)
+    .add('with title', () => (
+        <FullHeader title="TDD" />
     ))
-    .add('with another name', () => (
-        <Component name="another" />
-    ))
+    .add('with title and subtitle', () => {
+        <FullHeader title="TDD" subtitle="Curso de JavaScript com TDD na prática." />
+    })
+    .add('with title, subtitle, bgColor', () => {
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JavaScript com TDD na prática."
+            bgColor="#3299BB"/>
+    })
+    .add('with title, subtitle, bgColor, textColor', () => {
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JavaScript com TDD na prática."
+            bgColor="#3299BB"
+            textColor="#FF990"/>
+    })
+    .add('with title, subtitle, bgColor, textColor, font', () => {
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JavaScript com TDD na prática."
+            bgColor="#3299BB"
+            textColor="#FF990"
+            font="cursive"/>
+    })
+    .add('with title, subtitle, bgImg', () => {
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JavaScript com TDD na prática."
+            bgColor="#3299BB"
+            textColor="#FF990"
+            bgImg="https://raw.githubusercontent.com/willianjusten/photo-examples/master/iceland-glacier.jpg"/>
+    })
+    .add('with title, subtitle, video', () => {
+        <FullHeader
+            title="TDD"
+            subtitle="Curso de JavaScript com TDD na prática."
+            bgColor="#3299BB"
+            textColor="#FF990"
+            video="http://callmenick.com/_development/video/full-screen-background-video/media/demo.mp4"/>
+    })
+
