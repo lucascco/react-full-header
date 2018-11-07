bc9c40988845008098d3351058aa20587cee4b01
Lucas Carvalho <lucasccorrea@gmail.com>
2018-11-07 13:06:31 -0200

    add tests to subtitle

--- tests/specs/Main.spec.js
+++ tests/specs/Main.spec.js
@@ -9,18 +9,38 @@ describe('<FullHeader />', () => {
         expect(wrapper.find('header')).to.have.length(1);
     });
 
-    it('should have  h1 tag when title passed', () => {
-        const wrapper = shallow(<FullHeader title="TDD"/>);
-        expect(wrapper.find('h1')).to.have.length(1);
+    context('title', () => {
+        it('should have  h1 tag when title passed', () => {
+            const wrapper = shallow(<FullHeader title="TDD"/>);
+            expect(wrapper.find('h1')).to.have.length(1);
+        });
+
+        it('should no have h1 tag when title is not passed', () => {
+            const wrapper = shallow(<FullHeader/>);
+            expect(wrapper.find('h1')).to.have.length(0);
+        });
+
+        it('should have h1 tag with title when title is passed', () => {
+            const wrapper = shallow(<FullHeader title="TDD"/>);
+            expect(wrapper.find('h1').props().children).to.be.equal('TDD');
+        });
     });
 
-    it('should no have h1 tag when title is not passed', () => {
-        const wrapper = shallow(<FullHeader/>);
-        expect(wrapper.find('h1')).to.have.length(0);
-    });
+    context('subtitle', () => {
+        it('should have h2 tag when subtitle passed', () => {
+            const wrapper = shallow(<FullHeader title="TDD" subtitle="Curso de JS com TDD na Pratica"/>);
+            expect(wrapper.find('h2')).to.have.length(1);
+        });
 
-    it('should have h1 tag with title when title is passed', () => {
-        const wrapper = shallow(<FullHeader title="TDD"/>);
-        expect(wrapper.find('h1').props().children).to.be.equal('TDD');
+        it('should no have h2 tag when subtitle is not passed', () => {
+            const wrapper = shallow(<FullHeader title="TDD"/>);
+            expect(wrapper.find('h2')).to.have.length(0);
+        });
+
+
+        it('should have h2 tag with title when subtitle is passed', () => {
+            const wrapper = shallow(<FullHeader title="TDD" subtitle="Curso de JS com TDD na Pratica"/>);
+            expect(wrapper.find('h2').props().children).to.be.equal('Curso de JS com TDD na Pratica');
+        });
     });
 });
